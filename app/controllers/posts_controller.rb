7948a2aef7971ebf631800af02bbf55cda5a4f01
ST <st@STs-MacBook-Pro.local>
2020-12-06 07:36:48 +0900

    投稿編集機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def show
@@ -15,5 +15,17 @@ class PostsController < ApplicationController
     @post = Post.new(content:params[:content]) # インスタンスを作り、new.html.erbのフォームから入力されたcontentカラムのデータを変数に代入
     @post.save
     redirect_to("/posts/index")
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts/index")
+
   end
 end
