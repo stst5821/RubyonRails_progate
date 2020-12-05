68b9a55376ba80b163141ef3acd79ebbed74d68b
ST <st@STs-MacBook-Pro.local>
2020-12-06 07:37:24 +0900

    投稿編集機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get "posts/new"
   get "posts/:id" => "posts#show"
   post "posts/create"
-  
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
   get '/' => "home#top"
   get 'top' => "home#top"
   get 'about' => "home#about"
